620f291d36ae2268dee93a4738ea7a4396a7c6c2
Sizan <bsse1531@iit.du.ac.bd>
2026-01-27 05:51:21 +0600

    commit

---
+++ lib/config/app_routes.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import '../screens/splash_screen.dart';
+import '../screens/auth/login.dart';
+import '../screens/auth/register.dart';
+import '../screens/auth/verify_email.dart';
+import '../screens/auth/forgot_password.dart';
+import '../screens/home/home.dart';
+
+class AppRoutes {
+  static const String splash = '/';
+  static const String login = '/login';
+  static const String register = '/register';
+  static const String verifyEmail = '/verify-email';
+  static const String forgotPassword = '/forgot-password';
+  static const String home = '/home';
+
+  static Map<String, WidgetBuilder> getRoutes() {
+    return {
+      splash: (context) => const SplashScreen(),
+      login: (context) => const LoginScreen(),
+      register: (context) => const RegisterScreen(),
+      verifyEmail: (context) => const VerifyEmailScreen(),
+      forgotPassword: (context) => const ForgotPasswordScreen(),
+      // home: (context) => const HomeScreen(), // হোম স্ক্রিন বানালে এটি আনকমেন্ট করবেন
+    };
+  }
+}
